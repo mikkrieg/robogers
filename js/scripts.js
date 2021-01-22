4d78dc5a15df03a4b59a1bf1f6ccf93d549a5153
Mikey Kriegel <mikkrieg@gmail.com>
2021-01-22 09:32:43 -0800

    Create rangeOf function, passes first spec test

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,37 @@
 $(document).ready(function(){
 
   // Business Logic
+  const numberArray = [];
+
+  function rangeOf(input) {
+    for(let i = 0; i <= input; i++) {
+      const number = i;
+      numberArray.push(number);
+    }
+    console.log(numberArray.join(", "));
+  }
+
+ 
+
+
+
+
+
+
+
+
 
 
   $('form').submit(function(e){
     e.preventDefault();
 
+    const userInput = parseInt($('input#userNumber').val());
+
     //User Logic
-    function roboger(){
-
+    function roboger(input){
+      rangeOf(input)
     }
-
+    roboger(userInput)
     $("p#output-text").text();
     $('div#output').show();
   });
