751cd7158bdf8ae9e91b09ea381e80652167010b
Mikey Kriegel <mikkrieg@gmail.com>
2021-01-22 11:46:03 -0800

    Change checkOne to lookForFirst, continue building it

--- js/scripts.js
+++ js/scripts.js
@@ -10,54 +10,37 @@ $(document).ready(function(){
     }
   } 
 
-  
-  function lookForOne(){
+  function lookForFirst(){
     numberArray.forEach(function(number){
       const firstNumber = number[0];
       if(firstNumber === '1'){
         const replaced = number.replace(number, "Beep!");
-        console.log(replaced);
+
+      } else if(firstNumber === '2') {
+        const replaced = number.replace(number, "Boop!");
+
+      } else if(firstNumber === '3'){
+        const replaced = number.replace(number, "Won't you be my neighbor?");
+
       }
     });
   }
-  
-  
-  
-  
-    //   numberArray.forEach(function(number){
-  //     const splitString = number.split(" ").join();
-  //     splitArray.push(splitString);
-  //   });
-  //   console.log(splitArray);
-  //   for(let i = 0; i <= stringArray; i++){
-  //     console.log(stringArray);
-  //     if(splitString.substring(0, 1) === "1"){
-  //       const replaced = splitString.replace(stringArray[i], "Beep!");
-  //       console.log(replaced);
-  //       console.log(stringArray);
-  //       console.log("you've reached if")
-  //     }
-  //   }
-  // }
-  // console.log(numberArray);
-  // console.log(stringArray);
 
 
 
 
 
 
-
-
+  //User Interface Logic
   $('form').submit(function(e){
     e.preventDefault();
 
     const userInput = $('input#userNumber').val();
 
-    //User Logic
+   
     function roboger(input){
       rangeOf(input);
-      lookForOne();
+      lookForFirst();
     }
     roboger(userInput)
     $("p#output-text").text();
