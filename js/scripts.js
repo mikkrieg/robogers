dd049d6a5c8141efd62869b33e90ea46c916f937
Mikey Kriegel <mikkrieg@gmail.com>
2021-01-22 10:05:24 -0800

    Begin function that checks for one, almost complete

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,29 @@ $(document).ready(function(){
 
   // Business Logic
   const numberArray = [];
+  const stringArray = [];
 
   function rangeOf(input) {
     for(let i = 0; i <= input; i++) {
       const number = i;
       numberArray.push(number);
     }
-    console.log(numberArray.join(", "));
-  }
+  } 
 
- 
+  function lookForOne(){
+    for(number of numberArray){
+      const numberString = number.toString();
+      stringArray.push(numberString);
+    }
+    stringArray.forEach(function(string){
+      const splitString = string.split("").toString();
+      if(splitString.substring(0, 1) === "1"){
+        let one = splitString.substring(0, 1);
+        one = "Beep!";
+        console.log(one)
+      }
+    });
+  }
 
 
 
@@ -29,7 +42,8 @@ $(document).ready(function(){
 
     //User Logic
     function roboger(input){
-      rangeOf(input)
+      rangeOf(input);
+      lookForOne();
     }
     roboger(userInput)
     $("p#output-text").text();
