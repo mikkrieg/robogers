07726313934b9b25cd1be4725410ab8152671991
Mikey Kriegel <mikkrieg@gmail.com>
2021-01-22 09:24:18 -0800

    Create basic user interface logic

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+  // Business Logic
+
+
+  $('form').submit(function(e){
+    e.preventDefault();
+
+    //User Logic
+    function roboger(){
+
+    }
+
+    $("p#output-text").text();
+    $('div#output').show();
+  });
+});
